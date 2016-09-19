abdb155570ec071445ab637f9a7355b86a62107f
MichaelMGonzalez <mmg005@ucsd.edu>
2016-09-19 13:16:00 -0700

    Refactored directories

---
+++ Unity/SoundEffectPlayer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundEffectPlayer : MonoBehaviour {
+
+    public SoundCategory[] soundCategories;
+    public Dictionary<string, SoundCategory> soundMap;
+    AudioSource src;
+    void Start()
+    {
+        src = GetComponent<AudioSource>();
+        soundMap = new Dictionary<string, SoundCategory>();
+        foreach(SoundCategory c in soundCategories)
+        {
+            soundMap[c.name] = c;
+        }
+    }
+
+    public void PlaySound(string category)
+    {
+        if(!soundMap.ContainsKey(category))
+        {
+            Debug.LogError("Category " + category + "not found!");
+            return;
+        }
+        AudioClip clip = soundMap[category].GetRandomClip();
+        src.PlayOneShot(clip);
+    }
+	
+}
+
+
+[System.Serializable]
+public class SoundCategory : System.Object
+{
+    public string name = "INSERT_NAME";
+    public AudioClip[] clips;
+    public AudioClip GetRandomClip()
+    {
+        AudioClip rv = clips[Random.Range(0, clips.Length)];
+        return rv;
+    }
+}
