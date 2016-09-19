f1a68bb5570194bfa98f082d15a6a9af2a1611bd
MichaelMGonzalez <mmg005@ucsd.edu>
2016-09-18 20:30:36 -0700

    Added match rotation script

---
+++ MatchRotation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MatchRotation : MonoBehaviour {
+
+    public Transform other;
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.rotation = Quaternion.Slerp(transform.rotation, other.rotation, Time.fixedDeltaTime);
+	}
+}
