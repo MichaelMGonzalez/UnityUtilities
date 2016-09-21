56855b07ac90393366578b8ce876c0f0d242a0b9
Michael <mmg005@ucsd.edu>
2016-09-21 12:49:43 -0700

    Created ExceptionLogger

---
+++ Unity/ExceptionLogger.cs
@@ -0,0 +1,25 @@
+﻿#define EXCEPTION_LOGGER
+using UnityEngine.SceneManagement;
+using System;
+using System.IO;
+using UnityEngine;
+public class ExceptionLogger  {
+    public static string LogException(Exception e, string prependToString, object reason )
+    {
+        string rv = "LOGGING FAILED";
+        prependToString += " Exception Occured in Scene: " + SceneManager.GetActiveScene().name + "\n";
+        DateTime now = DateTime.Now;
+        string errorLogFileName = (Application.dataPath + "/Exceptions/");
+        errorLogFileName += now.Year + "_" + now.Month + "_" + now.Day + "/" + reason.GetType() + "/";
+        errorLogFileName = errorLogFileName.Replace("/", "\\");
+        try
+        {
+            Directory.CreateDirectory(errorLogFileName);
+            errorLogFileName += DateTime.Now.ToFileTime() + "_" + "(" + e.GetType() + ").txt";
+            File.WriteAllText(errorLogFileName, prependToString + e.StackTrace);
+            rv = errorLogFileName;
+        }
+        catch (Exception e2) { Debug.LogError("Could not create exceptions directory" + e2.GetType()); }
+        return rv;
+    }
+}
